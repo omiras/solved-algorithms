3375cc658c99193b0a6a0113362d4c236d149eb5
Oscar Miras <oscar.miras@gmail.com>
2022-05-26 12:23:03 +0200

    Create counting-amazon.js

---
+++ counting-amazon.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55b95c76e08bd5eef100001e
+
+function countArara(n) {
+    //your code here
+}
+
+console.log(countArara(1),"anane");
+console.log(countArara(3),"adak anane");
+console.log(countArara(8),"adak adak adak adak");
