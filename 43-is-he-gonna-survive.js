5b6e7f3be50fa76eb1354ddf3a7c8db12e9e7c2e
Oscar Miras <oscar.miras@gmail.com>
2023-04-18 13:53:19 +0200

    ejercicio David

---
+++ 43-is-he-gonna-survive.js
@@ -0,0 +1,16 @@
+// Kata: https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// Le el enunciado e implementa la función que piden
+// Recuerda explicar con tus palabras que debe hacer tu función antes de escribir una sola línea de JsvaScript
+// Usa ejemplos para describir qué debe hacer tu código
+
+function hero(bullets, dragons) {
+  //Get Coding!
+}
+
+console.log(hero(10, 5), true);
+console.log(hero(7, 4), false);
+console.log(hero(4, 5), false);
+console.log(hero(100, 40), true);
+console.log(hero(1500, 751), false);
+console.log(hero(0, 1), false);
