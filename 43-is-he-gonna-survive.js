587946ed6381cf22061e1c51d49b464a40a9a0de
David Pérez <devrap99@gmail.com>
2023-04-19 10:58:48 +0200

    43 Acabado y arreglado

--- 43-is-he-gonna-survive.js
+++ 43-is-he-gonna-survive.js
@@ -5,7 +5,22 @@
 // Usa ejemplos para describir qué debe hacer tu código
 
 function hero(bullets, dragons) {
-  //Get Coding!
+  // "bullets" va a hacer referencia a las balas disponibles que va a tener el heroe.
+  // "dragons" va a hacer referencia a la cantidad de dragones con la que se va a encontrar el heroe.
+  // "BulletsPerDragon" va a hacer referencia a la cantidad de balas que se van a necesitar para matar a cada dragón.
+  // Nos queda en claro que si el heroe tiene las balas suficientes (>=) para matar a todos los dragones se dará el valor boleano "true".
+  // En caso de que el heroe se quede sin balas y no pueda matar a todos los dragones se dará el valor boleano "false".
+
+  // Vamos a usar el primer caso como ejemplo.
+  // Si el heroes tiene 10 balas, hay 5 dragones que matar y se necesitan 2 balas por cada dragón... Entendemos que el valor boleano será "true", ya que, las 10 balas que tiene el heroe son iguales o mayores a las balas necesarias para matar a los 5 dragones (2*5=10).
+
+  let bulletsPerDragon = 2;
+
+  if (bullets >= bulletsPerDragon * dragons) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(hero(10, 5), true);
