ff4795b79fb20786a6508667f193fcb82a82e3ec
Oscar Miras <oscar.miras@gmail.com>
2023-04-19 12:33:12 +0200

    Create 44-sentence-smash.js

---
+++ 44-sentence-smash.js
@@ -0,0 +1,13 @@
+function smash(words) {
+   let sentence = "";
+  
+    /** -- Code your solution between the lines--  */
+  
+    /** ----------------- */
+  
+   return sentence;
+};
+
+console.log(smash(["hello", "world"])); // "hello world"
+console.log(smash(["hello", "amazing", "world"])); // "hello amazing world";
+console.log(smash(["this", "is", "a", "really", "long", "sentence"])); // "this is a really long sentence");
