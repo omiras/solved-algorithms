bd9fde071236c88d2a8cc4980bf28af64ba51953
javier aldana <jdad1701@gmail.com>
2023-04-24 22:42:12 +0200

    46 terminado :)

--- 46-mixed-array.js
+++ 46-mixed-array.js
@@ -5,11 +5,17 @@ function sumMix(x) {
 
   /** Codificar a partir de aquí */
 
+
+  for (let i = 0; i < x.length; i++) {
+//+= es un operador de asignación compuesta que añade el valor de la expresión del lado derecho a la variable del lado izquierdo.
+    suma += parseInt(x[i]);
+//parseInt() es una función en JavaScript que convierte un valor de cadena en un número entero.
+  }
   return suma;
 }
 
 // en el terminal verás:
-console.log(sumMix([9, 3, "7", "3"])); // 2
+console.log(sumMix([9, 3, "7", "3"])); // 22
 console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7])); //42
 console.log(sumMix(["3"])); // 3
 console.log(sumMix([])); // 0
