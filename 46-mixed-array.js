9930ad0a312386063e0f7a6be61177a3d5e140fb
Oscar Miras <oscar.miras@gmail.com>
2023-04-24 10:36:10 +0200

    ejercicio 46

---
+++ 46-mixed-array.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009/
+
+function sumMix(x) {
+  let suma = 0;
+
+  /** Codificar a partir de aquí */
+
+  return suma;
+}
+
+// en el terminal verás:
+assert.strictEqual(sumMix([9, 3, "7", "3"])); // 2
+assert.strictEqual(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7])); //42
+assert.strictEqual(sumMix(["3"])); // 3
+assert.strictEqual(sumMix([])); // 0
