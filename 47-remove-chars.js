c74eec2a78955d31258509abd92e17a0024836e4
Oscar Miras <oscar.miras@gmail.com>
2023-04-25 14:21:49 +0200

    Create 47-remove-chars.js

---
+++ 47-remove-chars.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+function removeChar(str){
+ //You got this!
+
+};
+
+console.log(removeChar('eloquent'), 'loquen');
+console.log(removeChar('country'), 'ountr');
+console.log(removeChar('pe'), '');
