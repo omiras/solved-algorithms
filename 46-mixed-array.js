a7bc95e3713cc1442562b149c7e18ccf6c8f4b02
javier aldana <jdad1701@gmail.com>
2023-04-25 08:58:14 +0200

    ahora suma decimales:)

--- 46-mixed-array.js
+++ 46-mixed-array.js
@@ -8,7 +8,8 @@ function sumMix(x) {
 // queremos sumar todo el array asi que escribimos un bucle que recorra todo el array
   for (let i = 0; i < x.length; i++) {
 //+= es un operador de asignación compuesta que añade el valor de la expresión del lado derecho a la variable del lado izquierdo.
-    suma += parseInt(x[i]);
+// solucion anterior: suma += parseInt(x[i]); (solo suma numeros enteros)
+    suma += +(x[i]);
 //parseInt() es una función en JavaScript que convierte un valor de cadena en un número entero.
   }
   return suma;
@@ -16,7 +17,7 @@ function sumMix(x) {
 
 // en el terminal verás:
 console.log(sumMix([9, 3, "7", "3"])); // 22
-console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7])); //42
+console.log(sumMix(["5", "0", 9, 3, 2, 1.2, "9", 6, 7])); //42
 console.log(sumMix(["3"])); // 3
 console.log(sumMix([])); // 0
 
