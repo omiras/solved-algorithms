f7257e3604b8013d54d270e864b3e7ea84b2036b
Oscar Miras <oscar.miras@gmail.com>
2023-05-02 14:24:10 +0200

    Update 50-small-enough.js

--- 50-small-enough.js
+++ 50-small-enough.js
@@ -4,7 +4,7 @@ function smallEnough(a, limit){
   
 }
 
-console.log(smallEnough([66, 101], 200));
-console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
-console.log(smallEnough([101, 45, 75, 105, 99, 107], 107));
-console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
+console.log(smallEnough([66, 101], 200)); //true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); // true
