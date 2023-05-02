84da7bb29446bc73e8c06a8da1fc87186d478847
Oscar Miras <oscar.miras@gmail.com>
2023-05-02 09:53:44 +0200

    Create small-enough.js

---
+++ small-enough.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57cc981a58da9e302a000214/
+
+function smallEnough(a, limit){
+  
+}
+
+console.log(smallEnough([66, 101], 200));
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107));
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
