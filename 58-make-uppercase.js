ec4c38c62fa4d05d185f5bacf79379cc2c3d260a
Oscar Miras <oscar.miras@gmail.com>
2023-10-17 13:40:29 +0200

    Create 58-make-uppercase.js

---
+++ 58-make-uppercase.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+function makeUpperCase(str) {
+  // Code here
+}
+
+console.log(makeUpperCae("hello")); // "HELLO"
+console.log(makeUpperCae("Patata")); // "PATATA"
