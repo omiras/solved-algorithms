a1940668a9acd0d551723163d3ddf717c40b77fd
Dmucci <dmucci93@gmail.com>
2023-10-19 22:59:12 +0200

    56uefaEuro2016 Finalizado

--- 56-uefa-euro-2016.js
+++ 56-uefa-euro-2016.js
@@ -10,8 +10,24 @@
 
 function uefaEuro2016(teams, scores) {
   // your code...
+  
+  if (scores[0]>scores[1]) {
+    return 'At match ' + teams[0] + '-' + teams[1] + ',' + teams[0] + 'won!';
+  //si los goles del primer equipo son mayores que los del segundo equipo, diremos que ha ganado Alemania.
+  } else if (scores[1]>scores[0]) {
+    return 'At match ' + teams[0] + '-' + teams[1] + ',' + teams[1] + 'won!';
+  //si los goles del segundo equipo son mayores que los del primer equipo, diremos que ha ganado Italia.
+  } else if (scores[0] == scores[1]) {
+    return 'At match ' + teams[0] + '-' + teams[1] + ', teams played draw';
+  //si los goles del primer equipo son igual a los del segundo equipo, diremos que ha habido un empate.
+  }
 }
 
-uefaEuro2016(["Germany", "Ukraine"], [2, 0]); // "At match Germany - Ukraine, Germany won!"
-uefaEuro2016(["Belgium", "Italy"], [0, 2]); // "At match Belgium - Italy, Italy won!"
-uefaEuro2016(["Portugal", "Iceland"], [1, 1]); // "At match Portugal - Iceland, teams played draw."
+console.log(uefaEuro2016(["Germany", "Ukraine"], [2, 0])); 
+// uefaEuro2016(["Germany", "Ukraine"], [2, 0]); // "At match Germany - Ukraine, Germany won!"
+console.log(uefaEuro2016(["Belgium", "Italy"], [0, 2]));
+//  uefaEuro2016(["Belgium", "Italy"], [0, 2]); // "At match Belgium - Italy, Italy won!"
+console.log(uefaEuro2016(["Portugal", "Iceland"], [1, 1])); 
+// uefaEuro2016(["Portugal", "Iceland"], [1, 1]); // "At match Portugal - Iceland, teams played draw."
+
+// console.log(uefaEuro2016(teams,scores));
