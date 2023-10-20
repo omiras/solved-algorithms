7f64c7a2d36aae7610bb03ae4aca3ff361f6c1a7
Oscar Miras <oscar.miras@gmail.com>
2023-10-20 16:56:53 +0200

    Update 57-first-non-conescutive.js

--- 57-first-non-conescutive.js
+++ 57-first-non-conescutive.js
@@ -9,6 +9,7 @@ P.ej. Si tenemos un array [1,2,3,4,6,7,8] entonces 1, luego 2, luego 3, luego 4 
 Si toda el array es consecutivo, devuelve el valor null. Significará que esta cadena de ADN no contiene errores.
 
 El array puede ser de números positivos o negativos, pero al menos, tendrá dos elementos. 
+Nos aseguran también que cada array SOLAMENTE contiene un número descolocado, es decir, que no esta en sequencia.
  * 
  * @param {array} arr Array de números 
  * @returns Devuelve un número o null
@@ -21,4 +22,4 @@ function firstNonConsecutive(arr) {
 
 console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // 6
 console.log(firstNonConsecutive([1, 2, 3, 4])); // null (porque todos son consecutivos)
-console.log(firstNonConsecutive([-10, -5, -1])); // -1
+console.log(firstNonConsecutive([-4, -3, -1])); // -1
