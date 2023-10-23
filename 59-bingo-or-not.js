fa4997dc570db01c6f25a9f01a9cdc3ef2981e9c
Oscar Miras <oscar.miras@gmail.com>
2023-10-23 17:29:53 +0200

    Update 59-bingo-or-not.js

--- 59-bingo-or-not.js
+++ 59-bingo-or-not.js
@@ -13,4 +13,4 @@ function bingo(numbers) {
 
 console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // "LOSE"
 console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10])); // "WIN"
-console.log(bingo([21, 13, 1, 7, 5, 14, 7, 15, 9, 10])); // "WIN"
+console.log(bingo([21, 13, 1, 7, 5, 14, 7, 15, 9, 10])); // "LOSE"
