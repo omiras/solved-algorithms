a8303322229cd99f12af0ab0f152237b77c494e5
omiras <oscar.miras@gmail.com>
2024-04-26 18:21:32 +0200

    exercises Zulema and Ricardo

---
+++ 62-print-errors.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * 1. Leer enunciado y entenderlo: https://www.codewars.com/kata/56541980fa08ab47a0000040/
+ * 
+ * 2. Plantear con tus propias palabras que debe hacer esta función
+ * 
+ * 3. Implementarlo. Por favor, usa un "for" aunque conozcas otras formas de hacerlo
+ * 
+ * Ayuda: Va a ser importante aprender dos cosas
+ * 
+ * 1. Como saber si una letra esta ocupa una posición 
+ * 2. Jugar adecuadamente con los índices para comparar una letra con otra
+ * 
+ */
+
+function printerError(s) {
+    // your code
+    let errors = "";
+
+    return errors;
+}
+
+console.log(printerError("aaabbbbhaijjjm")); // Debería devolver "0/14"
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm")); // Debería devolver "8/22"
+console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")); //  Debería devolver "3/56"
---
+++ 63-get-average.js
@@ -0,0 +1,25 @@
+/**
+ * 1. Entender el enunciado: https://www.codewars.com/kata/563e320cee5dddcf77000158
+ * 
+ * Básicamente Laia me ha pedido que implemente una función que, dada las notas de la clase, calcule la nota media
+ * 
+ * BONUS: Debes redonder el resultado hacía abajo
+ * 
+ * 2. Escribe con tus propias palabras que debe hacer esta función con ese array de numbers que recibe
+ * 
+ * 3. Implementa la función. 
+ * 
+ * Pista: Busca en Internet como obtener la media de un conjunto de números. Para el BONUS, busca como redondear en JavaScript
+ * 
+ */
+
+function getAverage(marks) {
+    let result = 0;
+
+    return result;
+
+}
+
+console.log(getAverage[2, 2, 2, 2]); // Debería devolver un 2
+console.log(getAverage([1, 2, 3, 4, 5])); // Debería devolver un 3
+console.log(getAverage([5, 5, 5, 5])); // Debería devolver un 5
