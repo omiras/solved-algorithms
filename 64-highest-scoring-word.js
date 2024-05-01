bbbd14aa879b4eef2d6f936fa5b389363b5d5193
omiras <oscar.miras@gmail.com>
2024-05-01 21:57:22 +0200

    exercises for Esteban and Andri

---
+++ 64-highest-scoring-word.js
@@ -0,0 +1,49 @@
+/**
+ * Estamos jugando a un juego parecido al Scrabble  
+ * 
+ * Nos han definido un sistema de puntuación en https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
+ * 
+ * El algoritmo debe devolver la palabra que más puntúa de todo el string .
+ */
+
+function high(x) {
+
+}
+
+console.log(high('man i need a taxi up to ubud')); // Valor esperado: 'taxi'
+console.log(high('what time are we climbing up the volcano')); // Valor esperado: 'volcano'
+console.log(high('take me to semynak')); // Valor esperado: 'semynak'
+console.log(high('aa b')); // Valor esperado: 'aa'
+
+
+/**
+ * PISTAS: 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 1. Usa el método .split() para separar cómodamente el string en un array
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 2. Usa una estructura de datos auxiliar para guardar la puntiación. La solución no son 24 if..else if....
+ */
+
