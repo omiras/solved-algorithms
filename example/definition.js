6ffbfb2a5681d5dab2bcb967c3a684965cdf0c59
omiras <oscar.miras@gmail.com>
2025-03-27 22:00:42 +0100

    done

--- example/definition.js
+++ example/definition.js
@@ -2,7 +2,11 @@
 
 
 function positiveSum(arr) {
-
+    // Tenemos que sumar todos los elementos del array
+      // En la variable "result" voy a acumular el resultado de mi operacion
+      // A cada elemento del array compruebo si es positivo o no (es decir, mayor a 0). Si lo es, lo suma en una 
+      
+    // al final devuelvo la variable "result"
 }
 
 console.log(positiveSum([1, 2])); // 3
--- example/implementation.js
+++ example/implementation.js
@@ -2,7 +2,18 @@
 
 
 function positiveSum(arr) {
+    // variable para almacenar el resultado
+    let result = 0;
 
+    // recorro el array
+    for (const n of arr) {
+        // si es mayor a 0, lo sumo
+        if (n>0) {
+            result = result + n
+        }
+    }
+
+    return result;
 }
 
 console.log(positiveSum([1, 2])); // 3
