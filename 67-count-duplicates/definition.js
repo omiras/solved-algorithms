693ee543b76ac0c37fbf7fb160b56be1ad1d2d81
ErebosXYZ <robertmaynou@gmail.com>
2025-03-30 13:08:50 +0200

    done

--- 67-count-duplicates/definition.js
+++ 67-count-duplicates/definition.js
@@ -1,7 +1,22 @@
 // URL: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
 
-function duplicateCount(text){
+function duplicateCount(text) {
     /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí.  */
+    /**
+    La funció recorre la string text. És convenient fer que aquesta estigui .toLowerCase.
+    Ha de retornar el nombre de lletres diferents que es repeteixen (si a es repeteix i b també, retornarà 2)
+    Això vol dir que si un dels índexs es repeteix, és a dir una lletra es troba a més d'una posició, la variable counter s'incrementa.
+    Aquesta pot ser un objecte en el qual li anem afegint keys que siguin els diferents caràcters.
+    Crear variable = {} que compti els caràcters de text
+
+    Si aquest objecte té caràcters repetits necessitem una altra variable que els guardi.
+    Aquestes variables són objectes buits. Creem un loop i dins d'aquest fem que busqui la variable char de la string que li donem.
+    characterCounter conté cada caràcter que compta el loop. Cada nou caràcter de la string suma 1 i els que no apareixen tenen un valor de 0.
+    Si algun dels caràcters de characterCounter apareix més d'un cop el guardem a repeatedCharacters amb el nombre de repeticions. És a dir, rpeatedCharacters guarda les vegades que algun dels caràcters es repeteix.
+    Finalment només és qüestió de retornar la llargada de les keys que té l'objecte repeatedCharacters. Si té 2 keys, vol dir que s'han repetit dues lletres.
+    */
+
+
 }
 
 console.log(duplicateCount("abcde")); // 0
