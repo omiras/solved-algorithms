f6d93a785dd219b655a8835b093d8e223d42ed5c
Oscar Miras <oscar.miras@gmail.com>
2025-04-02 10:43:23 +0200

    Update definition.js

--- 68-isogram/definition.js
+++ 68-isogram/definition.js
@@ -1,6 +1,6 @@
 
 // Lee el enunciado del siguiente algoritmo. 
-// URL: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+// URL:https://www.codewars.com/kata/54ba84be607a92aa900000f1
 
 
 function isIsogram(str){
