ebf4999760e7b8ba13aa9d02d7e12e0f9ba1cb39
alenorgue <alenoriegag@gmail.com>
2025-04-03 11:01:20 +0200

    finished

--- 68-isogram/definition.js
+++ 68-isogram/definition.js
@@ -1,10 +1,33 @@
 
 // Lee el enunciado del siguiente algoritmo. 
 // URL:https://www.codewars.com/kata/54ba84be607a92aa900000f1
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string 
+// that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
+//Example: (Input --> Output)
+
+//"Dermatoglyphics" --> true
+//"aba" --> false
+//"moOse" --> false (ignore letter case)
 
 function isIsogram(str){
-    /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí. También te puedes valer de dibujos o esquemas para enseñarlos a clase. NO escribir código JavaScript  */
+    /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí. 
+     * También te puedes valer de dibujos o esquemas para enseñarlos a clase. NO escribir código JavaScript  */
+
+
+//Necesitas comprobar si alguna letra se repite o no, si no se repite ninguna letra --> la funcion deberia devolver el valor true (Es un Isograma), 
+// si alguna letra se repite deberia devolver --> False ( No es in isograma).
+
+// Primero introducimos una funcion para convertir el string a lowerCase antes de comprobar (Para que no sea case sensitive)
+
+//Vamos a necesitar una funcion que recorra el string y guarde el [index] y lo compare con el resto del string. 
+
+//Y una variable que  acumule en un contador el numero de caracteres iguales, si el valor de la variable es >= de 2 --> False
+// let i = 0; i < str.length; i++) {
+//    let char = str[i];
+//    let contador = 0;
+
+//for (let j = 0; j < str.length; j++) { //Recorre el string
 }
 
 console.log(isIsogram("Dermatoglyphics")); // true
--- 68-isogram/implementation.js
+++ 68-isogram/implementation.js
@@ -2,8 +2,24 @@
 // URL: https://www.codewars.com/kata/54ba84be607a92aa900000f1
 
 
-function isIsogram(str){
+function isIsogram(str) {
     /** Recuerda que si usas estructuras o herramientas que no hemso visto en clase es posible que tus compañeros y compañeras te pregunten por ellas :) */
+    str = str.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i]; //Variable con el Character a comparar
+        let contador = 0;
+
+        for (let j = 0; j < str.length; j++) { //Recorre el string
+            if (str[j] === char) { 
+                contador++; } //Suma uno al contador
+            if (contador >= 2) {
+                return false; 
+            }
+
+        }
+    }
+    return true;
 }
 
 console.log(isIsogram("Dermatoglyphics")); // true
