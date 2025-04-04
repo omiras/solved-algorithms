0f386c172ea18d31e68fcda7e506ca50a8571162
SergioCaMi <sergiocami84@gmail.com>
2025-04-04 11:46:08 +0200

    68-isogram

--- 68-isogram/definition.js
+++ 68-isogram/definition.js
@@ -5,6 +5,16 @@
 
 function isIsogram(str){
     /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí. También te puedes valer de dibujos o esquemas para enseñarlos a clase. NO escribir código JavaScript  */
+    // Recibe un texto.
+    // leemos la primera letra y comprobamos si está guardada
+        // Como es la primera, no estará guardada y continuamos
+    // leemos la segunda letra y comprobamos que no está en las letras guardadas.
+        // En caso de que sí esté, cortamos las comprobaciones y devolvemos como valor que es FALSO el isograma recibido.
+        // En caso de que no, la añadimos como revisada
+    // leemos la tercera letra y comprobamos que no está en las letras guardadas...
+        // En caso de que sí esté, cortamos las comprobaciones y devolvemos como valor que es FALSO el isograma recibido.
+        // En caso de que no, la añadimos como revisada
+        // En caso de que hayamos leído todo el texto y no hayamos encontrada coincidencias, devolveremos que es VERDADERO el isograma.
 }
 
 console.log(isIsogram("Dermatoglyphics")); // true
--- 68-isogram/implementation.js
+++ 68-isogram/implementation.js
@@ -1,9 +1,18 @@
-// Lee el enunciado del siguiente algoritmo. 
+// Lee el enunciado del siguiente algoritmo.
 // URL: https://www.codewars.com/kata/54ba84be607a92aa900000f1
 
-
-function isIsogram(str){
-    /** Recuerda que si usas estructuras o herramientas que no hemso visto en clase es posible que tus compañeros y compañeras te pregunten por ellas :) */
+function isIsogram(str) {
+  /** Recuerda que si usas estructuras o herramientas que no hemso visto en clase es posible que tus compañeros y compañeras te pregunten por ellas :) */
+  const strRepeat = {};
+  str = str.toLowerCase();
+  for (let character of str) {
+    if (strRepeat[character]) {
+      return false;
+    } else {
+      strRepeat[character] = true;
+    }
+  }
+  return true;
 }
 
 console.log(isIsogram("Dermatoglyphics")); // true
