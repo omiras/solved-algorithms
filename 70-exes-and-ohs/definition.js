a6a183ec0dfb19e7cb22fdeb852ed0d0a42ab404
omiras <oscar.miras@gmail.com>
2025-04-08 11:45:55 +0200

    done

---
+++ 70-exes-and-ohs/definition.js
@@ -0,0 +1,13 @@
+// Lee el enunciado del siguiente algoritmo. 
+// URL: https://www.codewars.com/kata/55908aad6620c066bc00002a - Kyu 7
+
+
+function XO(str) {
+    /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí. También te puedes valer de dibujos o esquemas para enseñarlos a clase. NO escribir código JavaScript  */
+}
+
+XO('xo');                  // true
+XO('XO');                  // true
+XO('xxxoo');               // false
+XO('xxOo');                // true
+XO(''); // true
---
+++ 70-exes-and-ohs/implementation.js
@@ -0,0 +1,14 @@
+// Lee el enunciado del siguiente algoritmo. 
+// URL: https://www.codewars.com/kata/55908aad6620c066bc00002a - Kyu 7
+
+
+function XO(str) {
+    /** Recuerda que si usas estructuras o herramientas que no hemso visto en clase es posible que tus compañeros y compañeras te pregunten por ellas :) */
+}
+
+XO('xo');                  // true
+XO('XO');                  // true
+XO('xxxoo');               // false
+XO('xxOo');                // true
+XO(''); // true
+
